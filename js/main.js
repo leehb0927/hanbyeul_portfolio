efe4ef881ee9b91ce70443990276072346e1d660
leehb0927 <as352@naver.com>
2022-02-18 16:20:30 +0900

    포트폴리오- 첫 업로드

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var myFullpage = new fullpage('#fullpage', {
+    menu: '#menu',
+    anchors: ['section1', 'section2', 'section3' , 'section4', 'section5','section6','section7'],
+    scrollBar:true,
+});
+
+
+
+
+
+
+$(document).ready(function(){
+	
+    $("#skillbar_pho").animate({width:'93%'},1500);
+    $("#skillbar_html").animate({width:'83%'},1500);
+    $("#skillbar_css").animate({width:'83%'},1500);
+    $("#skillbar_js").animate({width:'65%'},1500);
+});
+/*skill bar*/
+
+new WOW().init();
+/*페이지 내림과 동시에 애니메이션 작동 */
+
+
+$('.slider_clock').EasySlides({
+    'show': 15,
+    'autoplay':true,
+    })
+
+    
